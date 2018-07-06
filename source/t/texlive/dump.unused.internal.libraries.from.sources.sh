d880dd2b82a72946ef2bfc89a5c8164687878c11
Patrick J Volkerding <volkerdi@slackware.com>
2018-07-06 03:15:37 +0000

    Fri Jul  6 03:15:37 UTC 2018
    t/texlive-2018.180630-x86_64-1.txz:  Upgraded.
      Thanks to Johannes Schoepfer.
    x/libXScrnSaver-1.2.3-x86_64-1.txz:  Upgraded.
    x/libXinerama-1.1.4-x86_64-1.txz:  Upgraded.
    x/libXxf86misc-1.0.4-x86_64-1.txz:  Upgraded.

--- source/t/texlive/dump.unused.internal.libraries.from.sources.sh
+++ source/t/texlive/dump.unused.internal.libraries.from.sources.sh
@@ -7,9 +7,9 @@ tar xf texlive-${VERSION}-source.tar.xz || exit 1
 mv texlive-${VERSION}-source.tar.xz texlive-${VERSION}-source.tar.xz.orig
 ( cd texlive-${VERSION}-source/libs && rm -rf cairo freetype2 gd gmp mpfr icu libpng pixman zlib )
 ( cd texlive-${VERSION}-source/utils && rm -rf asymptote )
+( cd texlive-${VERSION}-source/texk/texlive && rm -rf w*_wrapper )
 tar cf texlive-${VERSION}-source.tar texlive-${VERSION}-source
 rm -r texlive-${VERSION}-source
-#xz -9 texlive-${VERSION}-source.tar
-xz texlive-${VERSION}-source.tar
+xz -9 texlive-${VERSION}-source.tar
 touch -r texlive-${VERSION}-source.tar.xz.orig texlive-${VERSION}-source.tar.xz
 rm texlive-${VERSION}-source.tar.xz.orig

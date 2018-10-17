8073ad053c0e5783867cb5d9f64eb4cd05c31392
Patrick J Volkerding <volkerdi@slackware.com>
2018-10-17 03:11:25 +0000

    Wed Oct 17 03:11:25 UTC 2018
    a/bin-11.1-x86_64-3.txz:  Rebuilt.
      Renamed mktemp to mktemp-debianutils.
      Removed bban and sysvbanner which seem to be lacking proper licensing info.
    a/coreutils-8.30-x86_64-2.txz:  Rebuilt.
      Renamed mktemp-gnu to mktemp.
    a/xfsprogs-4.18.0-x86_64-2.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    ap/alsa-utils-1.1.7-x86_64-1.txz:  Upgraded.
    ap/sqlite-3.25.2-x86_64-2.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    d/ccache-3.5-x86_64-1.txz:  Upgraded.
    kde/calligra-2.9.11-x86_64-26.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    l/alsa-lib-1.1.7-x86_64-1.txz:  Upgraded.
    l/alsa-plugins-1.1.7-x86_64-1.txz:  Upgraded.
    l/boost-1.68.0-x86_64-2.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    l/harfbuzz-1.9.0-x86_64-2.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    l/icu4c-63.1-x86_64-1.txz:  Upgraded.
      Shared library .so-version bump.
      Those of you using Plasma 5 or various third party packages should know by
      now what it means when icu4c gets a bump in -current. You may want to wait
      a few days for those things to appear again rebuilt against this.
    l/libical-3.0.4-x86_64-1.txz:  Upgraded.
      Compiled against icu4c-63.1.
    l/libssh-0.7.6-x86_64-1.txz:  Upgraded.
      Fixed authentication bypass vulnerability.
      For more information, see:
        https://www.libssh.org/2018/10/16/libssh-0-8-4-and-0-7-6-security-and-bugfix-release/
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-10933
      (* Security fix *)
    l/libvisio-0.1.6-x86_64-5.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    l/qt-4.8.7-x86_64-11.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    l/raptor2-2.0.15-x86_64-6.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    n/dovecot-2.3.3-x86_64-2.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    n/php-7.2.11-x86_64-1.txz:  Upgraded.
      Compiled against icu4c-63.1.
    n/postfix-3.3.1-x86_64-3.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    n/tin-2.4.2-x86_64-4.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    t/texlive-2018.180822-x86_64-2.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    x/libdrm-2.4.96-x86_64-1.txz:  Upgraded.
    xfce/exo-0.12.3-x86_64-1.txz:  Upgraded.
    extra/brltty/brltty-5.6-x86_64-2.txz:  Rebuilt.
      Recompiled against icu4c-63.1.
    extra/pure-alsa-system/alsa-lib-1.1.7-x86_64-1_alsa.txz:  Upgraded.
    extra/pure-alsa-system/alsa-plugins-1.1.7-x86_64-1_alsa.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.2.10.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.2.10.tar.xz
+lftpget http://us.php.net/distributions/php-7.2.11.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.2.11.tar.xz

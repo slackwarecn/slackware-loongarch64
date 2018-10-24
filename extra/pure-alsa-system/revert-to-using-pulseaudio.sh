b80b3b22ebdcaa6151d82e43a56144b97a90d4fe
Patrick J Volkerding <volkerdi@slackware.com>
2018-10-24 21:22:04 +0000

    Wed Oct 24 21:22:04 UTC 2018
    d/strace-20181024_43700247-x86_64-1.txz:  Upgraded.
    l/alsa-plugins-1.1.7-x86_64-4.txz:  Rebuilt.
      Applied upstream fix for double free. Thanks to Jean-Philippe Guillemin.
    n/httpd-2.4.37-x86_64-1.txz:  Upgraded.
    n/mcabber-1.1.0-x86_64-1.txz:  Upgraded.
    x/libepoxy-1.5.3-x86_64-1.txz:  Upgraded.
    xap/pan-0.145-x86_64-1.txz:  Upgraded.
    extra/pure-alsa-system/alsa-plugins-1.1.7-x86_64-4_alsa.txz:  Rebuilt.
      Applied upstream fix for double free. Thanks to Jean-Philippe Guillemin.

--- extra/pure-alsa-system/revert-to-using-pulseaudio.sh
+++ extra/pure-alsa-system/revert-to-using-pulseaudio.sh
@@ -10,5 +10,8 @@ for package in MPlayer alsa-lib alsa-plugins audacious-plugins ffmpeg \
   libcanberra mpg123 phonon sox xine-lib ; do
   upgradepkg --install-new --terse ../../slackware*/*/${package}-*.txz
 done
-mv /etc/asound.conf.new /etc/asound.conf
+echo "Moving /etc/asound.conf.new to /etc/asound.conf:"
+mv --verbose /etc/asound.conf.new /etc/asound.conf
+echo "Making /etc/rc.d/rc.alsa non-executable."
+chmod 644 /etc/rc.d/rc.alsa
 echo "System is converted back to using Pulseaudio."

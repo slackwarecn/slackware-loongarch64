e10adef4c2a06f02cd4970f0f6459c27e840f772
Patrick J Volkerding <volkerdi@slackware.com>
2018-11-14 00:00:07 +0000

    Wed Nov 14 00:00:07 UTC 2018
    a/aaa_terminfo-6.1_20181110-x86_64-1.txz:  Upgraded.
    a/kernel-generic-4.19.2-x86_64-1.txz:  Upgraded.
    a/kernel-huge-4.19.2-x86_64-1.txz:  Upgraded.
    a/kernel-modules-4.19.2-x86_64-1.txz:  Upgraded.
    ap/vim-8.1.0526-x86_64-1.txz:  Upgraded.
      Don't package .desktop files for the base vim package.
      Move the terminal-based vim.desktop (menu clutter) into the docs directory.
    d/kernel-headers-4.19.2-x86-1.txz:  Upgraded.
    k/kernel-source-4.19.2-noarch-1.txz:  Upgraded.
    l/librsvg-2.44.9-x86_64-1.txz:  Upgraded.
    l/ncurses-6.1_20181110-x86_64-1.txz:  Upgraded.
      The new kernel fixes the artifact on the virtual console, so the original
      linux terminal definition has been restored.
    xap/vim-gvim-8.1.0526-x86_64-1.txz:  Upgraded.
      Let vim install its own icon and gvim.desktop files. Thanks to drumz.
      Don't include the terminal-based vim.desktop file.
    isolinux/initrd.img:  Rebuilt.
    kernels/*:  Upgraded.
    usb-and-pxe-installers/usbboot.img:  Rebuilt.

--- recompress.sh
+++ recompress.sh
@@ -727,7 +727,6 @@ gzip ./source/l/t1lib/t1lib_5.1.2-3.5.diff
 gzip ./source/l/ncurses/terminfo/xterm.terminfo.orig
 gzip ./source/l/ncurses/terminfo/xterm.terminfo.diff
 gzip ./source/l/ncurses/ncurses.mkhashsize.diff
-gzip ./source/l/ncurses/linux-no-bce.diff
 gzip ./source/l/ncurses/doinst.sh
 gzip ./source/l/clucene/clucene.install_contribs_lib.diff
 gzip ./source/l/clucene/clucene.pkgconfig.diff

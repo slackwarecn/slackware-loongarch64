beeee4227cedd89e351f22334d90e939fcded9a7
Patrick J Volkerding <volkerdi@slackware.com>
2018-12-08 05:23:51 +0000

    Sat Dec  8 05:23:51 UTC 2018
    pasture/php-5.6.39-x86_64-1.txz:  Upgraded.
      Several security bugs have been fixed in this release:
      Segfault when using convert.quoted-printable-encode filter.
      Null pointer dereference in imap_mail.
      imap_open allows to run arbitrary shell commands via mailbox parameter.
      PharData always creates new files with mode 0666.
      Heap Buffer Overflow (READ: 4) in phar_parse_pharfile.
      For more information, see:
        https://php.net/ChangeLog-5.php#5.6.39
      (* Security fix *)

--- pasture/source/php/fetch-php.sh
+++ pasture/source/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-5.6.38.tar.xz.asc
-lftpget http://us.php.net/distributions/php-5.6.38.tar.xz
+lftpget http://us.php.net/distributions/php-5.6.39.tar.xz.asc
+lftpget http://us.php.net/distributions/php-5.6.39.tar.xz

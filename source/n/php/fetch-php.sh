4dce2a46ca603e244644b58f7e18cf6af37589c3
Patrick J Volkerding <volkerdi@slackware.com>
2019-08-28 21:29:41 +0000

    Wed Aug 28 21:29:41 UTC 2019
    a/haveged-1.9.6-x86_64-1.txz:  Upgraded.
    a/sysvinit-scripts-2.1-noarch-27.txz:  Rebuilt.
      rc.6, rc.K: recognize partition type nfs4
      Thanks to Yoga the Yogin.
    ap/gutenprint-5.3.3-x86_64-1.txz:  Upgraded.
    ap/hplip-3.19.8-x86_64-1.txz:  Upgraded.
    ap/man-db-2.8.7-x86_64-1.txz:  Upgraded.
    d/meson-0.51.2-x86_64-1.txz:  Upgraded.
    d/ruby-2.6.4-x86_64-1.txz:  Upgraded.
    n/dovecot-2.3.7.2-x86_64-1.txz:  Upgraded.
    n/php-7.3.9-x86_64-1.txz:  Upgraded.
      This update fixes bugs and a security issue:
      MBString: Fixed bug #78380 (Oniguruma 6.9.3 fixes CVEs). (CVE-2019-13224)
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-13224
      (* Security fix *)

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.3.8.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.3.8.tar.xz
+lftpget http://us.php.net/distributions/php-7.3.9.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.3.9.tar.xz

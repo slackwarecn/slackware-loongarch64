f5ab9bf2e5270c884a30330bb58917f9e93234c2
Patrick J Volkerding <volkerdi@slackware.com>
2019-09-20 22:51:48 +0000

    Fri Sep 20 22:51:48 UTC 2019
    ap/qpdf-9.0.1-x86_64-1.txz:  Upgraded.
    l/glibmm-2.62.0-x86_64-1.txz:  Upgraded.
    l/libusb-1.0.23-x86_64-1.txz:  Upgraded.
    l/python-urllib3-1.25.5-x86_64-1.txz:  Upgraded.
    n/bluez-5.51-x86_64-1.txz:  Upgraded.

--- slackware64/l/gnome-themes-extra/doinst.sh
+++
@@ -1,8 +0,0 @@
-for i in HighContrast Adwaita ; do
-  if [ -e usr/share/icons/$theme/icon-theme.cache ]; then
-    if [ -x /usr/bin/gtk-update-icon-cache ]; then
-      /usr/bin/gtk-update-icon-cache usr/share/icons/$theme >/dev/null 2>&1
-    fi
-  fi
-done
-

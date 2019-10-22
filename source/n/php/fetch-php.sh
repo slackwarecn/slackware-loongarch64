ad0df123cf15a43a0d9f48bd7acd05d1dd5b1c39
Patrick J Volkerding <volkerdi@slackware.com>
2019-10-22 18:48:37 +0000

    Tue Oct 22 18:48:37 UTC 2019
    a/btrfs-progs-5.3-x86_64-1.txz:  Upgraded.
    a/kernel-firmware-20191022_2b016af-noarch-1.txz:  Upgraded.
    d/parallel-20191022-noarch-1.txz:  Upgraded.
    l/glib2-2.62.2-x86_64-1.txz:  Upgraded.
    l/python-pillow-6.2.1-x86_64-1.txz:  Upgraded.
    n/php-7.3.11-x86_64-1.txz:  Upgraded.
      This update fixes bugs and a security issue:
      FPM: env_path_info underflow in fpm_main.c can lead to RCE.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11043
      (* Security fix *)
    x/xkeyboard-config-2.28-noarch-1.txz:  Upgraded.
    xap/mozilla-firefox-68.2.0esr-x86_64-1.txz:  Upgraded.
      This release contains security fixes and improvements.
      For more information, see:
        https://www.mozilla.org/en-US/firefox/68.2.0/releasenotes/
        https://www.mozilla.org/security/known-vulnerabilities/firefoxESR.html
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-15903
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11757
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11758
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11759
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11760
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11761
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11762
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11763
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11764
      (* Security fix *)

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.3.10.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.3.10.tar.xz
+lftpget http://us.php.net/distributions/php-7.3.11.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.3.11.tar.xz

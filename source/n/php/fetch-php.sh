79ca536e7f4b3fad2036c77c36f32d09d9c27894
Patrick J Volkerding <volkerdi@slackware.com>
2019-12-01 19:45:45 +0000

    Sun Dec  1 19:45:45 UTC 2019
    ap/tmux-3.0a-x86_64-1.txz:  Upgraded.
    l/alsa-lib-1.2.1.2-x86_64-1.txz:  Upgraded.
    l/imagemagick-7.0.9_7-x86_64-1.txz:  Upgraded.
    l/oniguruma-6.9.4-x86_64-1.txz:  Added.
      This is a regular expressions library needed by PHP.
    n/mutt-1.13.0-x86_64-1.txz:  Upgraded.
    n/php-7.4.0-x86_64-1.txz:  Upgraded.
    extra/pure-alsa-system/alsa-lib-1.2.1.2-x86_64-1_alsa.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.3.12.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.3.12.tar.xz
+lftpget http://us.php.net/distributions/php-7.4.0.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.4.0.tar.xz

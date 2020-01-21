f6348b0bc1f332196f8a8c73fa6ba48ee3cb9310
Patrick J Volkerding <volkerdi@slackware.com>
2020-01-21 21:23:01 +0000

    Tue Jan 21 21:23:01 UTC 2020
    a/aaa_elflibs-15.0-x86_64-19.txz:  Rebuilt.
      Upgraded:  libcap.so.2.31, libgmp.so.10.4.0, libgmpxx.so.4.6.0.
      Added:  libgssapi_krb5.so.2.2, libk5crypto.so.3.1, libkrb5.so.3.3,
      libkrb5support.so.0.1.
    a/util-linux-2.35-x86_64-1.txz:  Upgraded.
    d/python-pip-20.0.1-x86_64-1.txz:  Upgraded.
    l/Mako-1.1.1-x86_64-1.txz:  Upgraded.
    l/keyutils-1.6.1-x86_64-1.txz:  Upgraded.
    n/krb5-1.17-x86_64-1.txz:  Added.
      Nothing links to this yet, but we'll need it soon enough. :-)
    n/php-7.4.2-x86_64-1.txz:  Upgraded.
      This update fixes bugs and security issues:
      Standard: OOB read in php_strip_tags_ex
      Mbstring: global buffer-overflow in 'mbfl_filt_conv_big5_wchar'
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-7059
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-7060
      (* Security fix *)
    n/samba-4.11.5-x86_64-1.txz:  Upgraded.
      This update fixes the following security issues:
      Replication of ACLs set to inherit down a subtree on AD Directory
      not automatic.
      Crash after failed character conversion at log level 3 or above.
      Use after free during DNS zone scavenging in Samba AD DC.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-14902
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-14907
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-19344
      (* Security fix *)
    xap/gparted-1.1.0-x86_64-1.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.4.1.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.4.1.tar.xz
+lftpget http://us.php.net/distributions/php-7.4.2.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.4.2.tar.xz

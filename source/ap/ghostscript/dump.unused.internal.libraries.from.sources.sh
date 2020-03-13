99054d5dbe96b4947262b6b28946c6b11c949e2b
Patrick J Volkerding <volkerdi@slackware.com>
2020-03-13 20:15:29 +0000

    Fri Mar 13 20:15:29 UTC 2020
    a/cryptsetup-2.3.1-x86_64-1.txz:  Upgraded.
    a/kernel-firmware-20200309_3b3dd5a-noarch-1.txz:  Upgraded.
    a/kernel-generic-5.4.25-x86_64-1.txz:  Upgraded.
    a/kernel-huge-5.4.25-x86_64-1.txz:  Upgraded.
    a/kernel-modules-5.4.25-x86_64-1.txz:  Upgraded.
    ap/ghostscript-9.51-x86_64-1.txz:  Upgraded.
    d/gcc-9.3.0-x86_64-1.txz:  Upgraded.
    d/gcc-brig-9.3.0-x86_64-1.txz:  Upgraded.
    d/gcc-g++-9.3.0-x86_64-1.txz:  Upgraded.
    d/gcc-gdc-9.3.0-x86_64-1.txz:  Upgraded.
    d/gcc-gfortran-9.3.0-x86_64-1.txz:  Upgraded.
    d/gcc-gnat-9.3.0-x86_64-1.txz:  Upgraded.
    d/gcc-go-9.3.0-x86_64-1.txz:  Upgraded.
    d/gcc-objc-9.3.0-x86_64-1.txz:  Upgraded.
    d/kernel-headers-5.4.25-x86-1.txz:  Upgraded.
    d/libtool-2.4.6-x86_64-13.txz:  Rebuilt.
      Recompiled to update embedded GCC version number.
    d/rust-1.42.0-x86_64-1.txz:  Upgraded.
    d/scons-3.1.2-x86_64-2.txz:  Rebuilt.
      Drop python2 support and switch to python3.
    k/kernel-source-5.4.25-noarch-1.txz:  Upgraded.
      GCC_VERSION 90200 -> 90300
    l/libuv-1.35.0-x86_64-1.txz:  Upgraded.
    n/NetworkManager-1.22.10-x86_64-1.txz:  Upgraded.
    n/postfix-3.4.10-x86_64-1.txz:  Upgraded.
    xap/blueman-2.1.2-x86_64-2.txz:  Rebuilt.
      This needed a rebuild for glib2-2.64.1. Thanks to Lockywolf.
    xap/mozilla-thunderbird-68.6.0-x86_64-1.txz:  Upgraded.
      This release contains security fixes and improvements.
      For more information, see:
        https://www.mozilla.org/en-US/thunderbird/68.6.0/releasenotes/
        https://www.mozilla.org/en-US/security/advisories/mfsa2020-10/
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-6805
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-6806
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-6807
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-6811
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-20503
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-6812
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-6814
      (* Security fix *)
    isolinux/initrd.img:  Rebuilt.
    kernels/*:  Upgraded.
    usb-and-pxe-installers/usbboot.img:  Rebuilt.

--- source/ap/ghostscript/dump.unused.internal.libraries.from.sources.sh
+++ source/ap/ghostscript/dump.unused.internal.libraries.from.sources.sh
@@ -8,6 +8,8 @@ VERSION=${VERSION:-$(echo ghostscript-*.tar.?z* | rev | cut -f 3- -d . | cut -f 
 tar xf ghostscript-${VERSION}.tar.xz || exit 1
 mv ghostscript-${VERSION}.tar.xz ghostscript-${VERSION}.tar.xz.orig
 ( cd ghostscript-${VERSION} && rm -rf freetype jpeg lcms2 lcms2art/doc/* libpng libtiff png tiff zlib )
+# Dump this huge PDF:
+( cd ghostscript-${VERSION} && find . -name GS9_Color_Management.pdf -exec rm {} \; )
 tar cf ghostscript-${VERSION}.tar ghostscript-${VERSION}
 rm -r ghostscript-${VERSION}
 plzip -9 -n 6 ghostscript-${VERSION}.tar

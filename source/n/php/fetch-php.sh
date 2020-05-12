823b6649c1dd888afcf5d2e599773b3ce4ccd804
Patrick J Volkerding <volkerdi@slackware.com>
2020-05-12 23:49:47 +0000

    Tue May 12 23:49:47 UTC 2020
    ap/mariadb-10.4.13-x86_64-1.txz:  Upgraded.
      This update fixes potential denial-of-service vulnerabilities.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2752
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2812
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2814
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2760
      (* Security fix *)
    ap/sudo-1.9.0-x86_64-1.txz:  Upgraded.
    l/harfbuzz-2.6.6-x86_64-1.txz:  Upgraded.
    l/tidy-html5-5.6.0-x86_64-1.txz:  Added.
    n/epic5-2.1.2-x86_64-1.txz:  Upgraded.
    n/php-7.4.6-x86_64-1.txz:  Upgraded.
      Built using --with-tidy=shared. Thanks to Thom1b.
      This update fixes bugs and security issues:
      Core: Long variables cause OOM and temp files are not cleaned.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2019-11048
      (* Security fix *)
    x/libmypaint-1.6.1-x86_64-1.txz:  Upgraded.
      Shared library .so-version bump.
    xap/gimp-2.10.18-x86_64-2.txz:  Rebuilt.
      Recompiled against libmypaint-1.6.1.
    testing/packages/PAM/mariadb-10.4.13-x86_64-1_pam.txz:  Upgraded.
      This update fixes potential denial-of-service vulnerabilities.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2752
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2812
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2814
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-2760
      (* Security fix *)
    testing/packages/PAM/sudo-1.9.0-x86_64-1_pam.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.4.5.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.4.5.tar.xz
+lftpget http://us.php.net/distributions/php-7.4.6.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.4.6.tar.xz

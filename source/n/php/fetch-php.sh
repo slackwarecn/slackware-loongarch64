d0dad0b9a20d04dea0b2bbd39ba851d67f877327
Patrick J Volkerding <volkerdi@slackware.com>
2020-07-07 20:51:59 +0000

    Tue Jul  7 20:51:59 UTC 2020
    l/alsa-lib-1.2.3.2-x86_64-1.txz:  Upgraded.
    l/dconf-editor-3.36.4-x86_64-1.txz:  Upgraded.
    l/fribidi-1.0.10-x86_64-1.txz:  Upgraded.
    l/libcap-2.38-x86_64-1.txz:  Upgraded.
    n/libgcrypt-1.8.6-x86_64-1.txz:  Upgraded.
    n/php-7.4.8-x86_64-1.txz:  Upgraded.
    n/rsync-3.2.2-x86_64-1.txz:  Upgraded.
    x/xterm-357-x86_64-1.txz:  Upgraded.
    xap/mozilla-firefox-78.0.1esr-x86_64-2.txz:  Rebuilt.
      Recompiled with --enable-default-toolkit=cairo-gtk3-wayland.
    xap/seamonkey-2.53.3-x86_64-1.txz:  Upgraded.
      This update contains security fixes and improvements.
      For more information, see:
        https://www.seamonkey-project.org/releases/seamonkey2.53.3
      (* Security fix *)
    extra/pure-alsa-system/alsa-lib-1.2.3.2-x86_64-1_alsa.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.4.7.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.4.7.tar.xz
+lftpget http://us.php.net/distributions/php-7.4.8.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.4.8.tar.xz

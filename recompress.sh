98e52ba8a56e076e38a00ec721f601c438c6a160
Patrick J Volkerding <volkerdi@slackware.com>
2020-07-20 17:37:10 +0000

    Mon Jul 20 17:37:10 UTC 2020
    a/kernel-firmware-20200716_1d1c80b-noarch-1.txz:  Upgraded.
    ap/mc-4.8.25-x86_64-1.txz:  Upgraded.
    d/nasm-2.15.03-x86_64-1.txz:  Upgraded.
    d/scons-4.0.1-x86_64-1.txz:  Upgraded.
    l/imagemagick-7.0.10_24-x86_64-1.txz:  Upgraded.
    l/jasper-2.0.18-x86_64-1.txz:  Upgraded.
    l/libcap-2.40-x86_64-1.txz:  Upgraded.
    l/libpsl-0.21.1-x86_64-2.txz:  Rebuilt.
    n/dnsmasq-2.82-x86_64-1.txz:  Upgraded.
    x/xev-1.2.4-x86_64-1.txz:  Upgraded.

--- recompress.sh
+++ recompress.sh
@@ -92,6 +92,7 @@ gzip ./source/d/make/make.glibc-2.27.glob.diff
 gzip ./source/d/make/make.guile30.diff
 gzip ./source/d/perl/perl.configure.multilib.patch
 gzip ./source/d/perl/libnet-3.08-Do-not-create-Net-libnet.cfg.patch
+gzip ./source/d/scons/scons.1
 gzip ./source/e/emacs/0001-Support-ImageMagick-version-7.patch
 gzip ./source/e/emacs/doinst.sh
 gzip ./source/tcl/tix/configure.private.diff
@@ -1044,7 +1045,6 @@ gzip ./source/l/svgalib/svgalib.prefix.diff
 gzip ./source/l/libexif/libexif.pc.diff
 gzip ./source/l/ilmbase/ilmbase-2.2.0-no_undefined.patch
 gzip ./source/l/qtscriptgenerator/include-everything.patch
-gzip ./source/l/libpsl/libpsl.gtk-doc-1.30.patch
 gzip ./source/l/shared-mime-info/doinst.sh
 gzip ./source/l/shared-mime-info/shared-mime-info.skip_tests.diff
 gzip ./source/l/glib/glib-1.2.10-gcc5.patch

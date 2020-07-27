3df47c8baadd98f1a0572144353c056b5ea7357f
Patrick J Volkerding <volkerdi@slackware.com>
2020-07-27 18:14:47 +0000

    Mon Jul 27 18:14:47 UTC 2020
    a/gettext-0.21-x86_64-1.txz:  Upgraded.
    d/bison-3.7-x86_64-1.txz:  Upgraded.
    d/gettext-tools-0.21-x86_64-1.txz:  Upgraded.
    n/postfix-3.5.6-x86_64-1.txz:  Upgraded.
    xap/mozilla-firefox-78.1.0esr-x86_64-1.txz:  Upgraded.
      This release contains security fixes and improvements.
      For more information, see:
        https://www.mozilla.org/en-US/firefox/78.1.0/releasenotes/
      (* Security fix *)

--- recompress.sh
+++ recompress.sh
@@ -1238,7 +1238,6 @@ gzip ./source/xap/mozilla-firefox/build-deps/autoconf/autoconf-2.13-consolidated
 gzip ./source/xap/mozilla-firefox/mimeTypes.rdf
 gzip ./source/xap/mozilla-firefox/mozilla-firefox-mimeTypes-fix.diff
 gzip ./source/xap/mozilla-firefox/unbreakdocs.diff
-gzip ./source/xap/mozilla-firefox/firefox.rust-1.45.diff
 gzip ./source/xap/electricsheep/electricsheep.mplayer.diff
 gzip ./source/installer/sources/initrd/etc/keymaps.tar
 gzip ./source/x/liberation-fonts-ttf/doinst.sh

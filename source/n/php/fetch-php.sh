f15476bf3e7de9998ebd2488dc769e2ea6b3d23d
Patrick J Volkerding <volkerdi@slackware.com>
2020-09-29 18:11:08 +0000

    Tue Sep 29 18:11:08 UTC 2020
    n/php-7.4.11-x86_64-1.txz:  Upgraded.
      This update fixes bugs and two security issues:
      Core: PHP parses encoded cookie names so malicious `__Host-` cookies
      can be sent.
      OpenSSL: Wrong ciphertext/tag in AES-CCM encryption for a 12 bytes IV.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-7070
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2020-7069
      (* Security fix *)
    x/mesa-20.2.0-x86_64-1.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.4.10.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.4.10.tar.xz
+lftpget http://us.php.net/distributions/php-7.4.11.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.4.11.tar.xz

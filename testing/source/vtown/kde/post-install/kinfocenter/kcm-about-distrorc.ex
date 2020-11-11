0e5d58329020be958ff760bd0d4fa0aec0146807
Patrick J Volkerding <volkerdi@slackware.com>
2020-11-11 04:04:47 +0000

    Wed Nov 11 04:04:47 UTC 2020
    testing/packages/vtown/kde/bluedevil-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/breeze-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/breeze-grub-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/breeze-gtk-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/drkonqi-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kactivitymanagerd-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kde-cli-tools-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kde-gtk-config-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kdecoration-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kdeplasma-addons-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kgamma5-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/khotkeys-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kinfocenter-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kmenuedit-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kscreen-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kscreenlocker-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/ksshaskpass-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/ksysguard-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kwallet-pam-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kwayland-integration-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kwayland-server-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kwin-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/kwrited-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/libkscreen-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/libksysguard-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/milou-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/oxygen-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-browser-integration-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-desktop-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-disks-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-integration-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-nm-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-pa-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-sdk-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-vault-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-workspace-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/plasma-workspace-wallpapers-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/polkit-kde-agent-1-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/powerdevil-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/sddm-kcm-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/systemsettings-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.
    testing/packages/vtown/kde/xdg-desktop-portal-kde-5.20.3-x86_64-1_vtown_1.txz:  Upgraded.

--- testing/source/vtown/kde/post-install/kinfocenter/kcm-about-distrorc.ex
+++ testing/source/vtown/kde/post-install/kinfocenter/kcm-about-distrorc.ex
@@ -1,5 +1,5 @@
 [General]
 LogoPath=/etc/kde/xdg/slackware_logo.png
 Website=http://www.slackware.com/
-Variant=Post 14.2 -current\n(packages by AlienBOB)
-
+# Commented out - just show what kinfocenter finds from /etc/os-release:
+#Variant=Post 14.2 -current\n(packages by AlienBOB)

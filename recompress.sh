272467a60bb9c300bdb0ba546ff821498ec27904
Patrick J Volkerding <volkerdi@slackware.com>
2020-11-16 21:23:09 +0000

    Mon Nov 16 21:23:09 UTC 2020
    a/util-linux-2.36.1-x86_64-1.txz:  Upgraded.
    l/imagemagick-7.0.10_38-x86_64-1.txz:  Upgraded.
    l/lz4-1.9.3-x86_64-1.txz:  Upgraded.
    l/mozjs78-78.5.0esr-x86_64-1.txz:  Upgraded.
    n/ModemManager-1.14.8-x86_64-1.txz:  Upgraded.
    n/mutt-2.0.1-x86_64-1.txz:  Upgraded.
    xap/mozilla-firefox-78.5.0esr-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://www.mozilla.org/en-US/firefox/78.5.0/releasenotes/

--- recompress.sh
+++ recompress.sh
@@ -1213,7 +1213,6 @@ gzip ./source/xap/mozilla-firefox/build-deps/autoconf/autoconf-2.13-consolidated
 gzip ./source/xap/mozilla-firefox/mimeTypes.rdf
 gzip ./source/xap/mozilla-firefox/mozilla-firefox-mimeTypes-fix.diff
 gzip ./source/xap/mozilla-firefox/unbreakdocs.diff
-gzip ./source/xap/mozilla-firefox/rust_1.47.0.patch
 gzip ./source/xap/electricsheep/electricsheep.mplayer.diff
 gzip ./source/installer/sources/initrd/etc/keymaps.tar
 gzip ./source/x/liberation-fonts-ttf/doinst.sh

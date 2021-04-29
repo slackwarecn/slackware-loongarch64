ded2ad645c217d20866979991ea57ca71b602f74
Patrick J Volkerding <volkerdi@slackware.com>
2021-04-29 18:49:00 +0000

    Thu Apr 29 18:49:00 UTC 2021
    a/less-581.2-x86_64-1.txz:  Upgraded.
    ap/nano-5.7-x86_64-1.txz:  Upgraded.
    d/cmake-3.20.2-x86_64-1.txz:  Upgraded.
    n/httpd-2.4.47-x86_64-1.txz:  Upgraded.
    n/samba-4.14.4-x86_64-1.txz:  Upgraded.
      This is a security release in order to address the following defect:
      Negative idmap cache entries can cause incorrect group entries in the
      Samba file server process token.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-20254
        https://www.samba.org/samba/security/CVE-2021-20254.html
      (* Security fix *)
    extra/php8/php8-8.0.5-x86_64-1.txz:  Upgraded.

--- extra/source/php8/fetch-php.sh
+++ extra/source/php8/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.0.3.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.0.3.tar.xz
+lftpget http://us.php.net/distributions/php-8.0.5.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.0.5.tar.xz

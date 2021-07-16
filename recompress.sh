eefcea89bf9bd1dc25244777d5e31d0fb48b9672
Patrick J Volkerding <volkerdi@slackware.com>
2021-07-16 18:53:33 +0000

    Fri Jul 16 18:53:33 UTC 2021
    a/mkinitrd-1.4.11-x86_64-25.txz:  Rebuilt.
      Abort if a temporary directory cannot be created, otherwise the contents of
      the current directory could be destroyed. Thanks to Andypoo.
    d/Cython-0.29.24-x86_64-1.txz:  Upgraded.
    l/libedit-20210714_3.1-x86_64-1.txz:  Upgraded.
    x/libwacom-1.11-x86_64-1.txz:  Upgraded.

--- recompress.sh
+++ recompress.sh
@@ -152,6 +152,7 @@ gzip ./source/a/gpm/inputattach.c
 gzip ./source/a/quota/quota.lwrap.needs.lnsl.diff
 gzip ./source/a/mkinitrd/mkinitrd.tempfile.to.mktemp.patch
 gzip ./source/a/mkinitrd/0001-Fix-LUKSTRIM-with-C-T-and-UUID.patch
+gzip ./source/a/mkinitrd/0002-bail-if-temp-dir-is-not-created.patch
 gzip ./source/a/mlocate/doinst.sh
 gzip ./source/a/file/file.xz.magic
 gzip ./source/a/file/file.zisofs.magic

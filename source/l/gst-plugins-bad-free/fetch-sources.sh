9e03634d298a015561dfa94c2f6ba892487e3f38
Patrick J Volkerding <volkerdi@slackware.com>
2021-09-25 19:32:08 +0000

    Sat Sep 25 19:32:08 UTC 2021
    a/coreutils-9.0-x86_64-2.txz:  Rebuilt.
      DIR_COLORS: Add support for .tzst, .zst, .flv, and .m2t extensions.
      Thanks to regdub.
    a/elvis-2.2_0-x86_64-8.txz:  Rebuilt.
      Removed elvis.clr.orig. Thanks to ctrlaltca.
    d/bison-3.8.2-x86_64-1.txz:  Upgraded.
    d/meson-0.59.1-x86_64-1.txz:  Upgraded.
    d/subversion-1.14.1-x86_64-4.txz:  Rebuilt.
      Added compile options --with-kwallet and --with-gnome-keyring (the latter was
      already used by default). Thanks to rsts and Windu.
    kde/plasma-desktop-5.22.5-x86_64-2.txz:  Rebuilt.
      Removed main.xml.orig. Thanks to ctrlaltca.
    l/gst-plugins-bad-free-1.18.5-x86_64-1.txz:  Added.
      These are the "bad" GStreamer plugins that have free enough licenses to ship.
      Thanks to Heinz Wiesinger.
    l/gtk4-4.4.0-x86_64-1.txz:  Upgraded.
    l/libssh2-1.10.0-x86_64-1.txz:  Upgraded.
    n/lynx-2.9.0dev.9-x86_64-1.txz:  Upgraded.
      Based on testing here, this seems stable enough to include. Please let me
      know if there are any new problems.
      lynx.cfg: Add ASSUME_CHARSET:utf-8. Thanks to usr345.
      Removed CHANGES.orig. Thanks to ctrlaltca.
    xap/x3270-4.0ga14-x86_64-1.txz:  Upgraded.
    xap/xpdf-4.03-x86_64-3.txz:  Rebuilt.
      Removed .orig files. Thanks to ctrlaltca.

---
+++ source/l/gst-plugins-bad-free/fetch-sources.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Copyright 2021  Patrick J. Volkerding, Sebeka, Minnesota, USA
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Call this script with the version of the gst-plugins-bad that you would
+# like to fetch the sources for. This will fetch the base source from
+# github, and then remove the non-free sources.
+#
+# Example:  VERSION=1.18.3 ./fetch-sources.sh
+
+VERSION=${VERSION:-1.18.5}
+
+rm -rf rm -rf gst-plugins-bad-free-$VERSION gst-plugins-bad-$VERSION
+
+wget https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-$VERSION.tar.xz
+
+tar xvf gst-plugins-bad-$VERSION.tar.xz
+rm -f gst-plugins-bad-$VERSION.tar.xz
+
+./gst-p-bad-cleanup.sh gst-plugins-bad-$VERSION
+
+mv gst-plugins-bad-$VERSION gst-plugins-bad-free-$VERSION
+
+tar cf gst-plugins-bad-free-$VERSION.tar gst-plugins-bad-free-$VERSION
+rm -rf gst-plugins-bad-free-$VERSION
+plzip -9 gst-plugins-bad-free-$VERSION.tar

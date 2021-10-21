1bb03caa091f9fa14d1e6120ad2bd1dfa25c76c5
Patrick J Volkerding <volkerdi@slackware.com>
2021-10-21 19:36:32 +0000

    Thu Oct 21 19:36:32 UTC 2021
    a/lvm2-2.03.13-x86_64-1.txz:  Upgraded.
      Reverted to working version.
    d/rust-1.56.0-x86_64-1.txz:  Upgraded.
    l/pipewire-0.3.39-x86_64-1.txz:  Upgraded.
    n/krb5-1.19.2-x86_64-2.txz:  Rebuilt.
      [PATCH] Fix KDC null deref on TGS inner body null server.
      This fixes an issue where an authenticated attacker can cause a denial of
      service in the KDC by sending a FAST TGS request with no server field.
      Thanks to nobodino.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-37750
      (* Security fix *)
    x/ibus-m17n-1.4.8-x86_64-1.txz:  Upgraded.
    x/libinput-1.19.2-x86_64-1.txz:  Upgraded.
    xap/freerdp-2.4.1-x86_64-1.txz:  Upgraded.
      This update fixes two security issues:
      Improper client input validation for gateway connections allows to overwrite
      memory.
      Improper region checks in all clients allow out of bound write to memory.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-41159
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-41160
      (* Security fix *)
    xap/gftp-2.7.1b-x86_64-1.txz:  Upgraded.
    extra/php8/php8-8.0.12-x86_64-1.txz:  Upgraded.
      This update fixes bugs and a security issue:
      FPM: PHP-FPM oob R/W in root process leading to privilege escalation.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-21703
      (* Security fix *)

--- extra/source/php8/fetch-php.sh
+++ extra/source/php8/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.0.11.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.0.11.tar.xz
+lftpget http://us.php.net/distributions/php-8.0.12.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.0.12.tar.xz

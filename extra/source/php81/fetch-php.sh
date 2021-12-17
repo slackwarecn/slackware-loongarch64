93a272f6d5fbe8ad33deccffedc172e527b7c329
Patrick J Volkerding <volkerdi@slackware.com>
2021-12-17 20:47:13 +0000

    Fri Dec 17 20:47:13 UTC 2021
    a/kernel-generic-5.15.10-x86_64-1.txz:  Upgraded.
    a/kernel-huge-5.15.10-x86_64-1.txz:  Upgraded.
    a/kernel-modules-5.15.10-x86_64-1.txz:  Upgraded.
    ap/inxi-3.3.11_1-noarch-1.txz:  Upgraded.
    ap/ksh93-1.0_20211217_ce3e080c-x86_64-1.txz:  Upgraded.
    ap/neofetch-20211210_ccd5d9f5-noarch-1.txz:  Upgraded.
    d/Cython-0.29.26-x86_64-1.txz:  Upgraded.
    d/kernel-headers-5.15.10-x86-1.txz:  Upgraded.
    k/kernel-source-5.15.10-noarch-1.txz:  Upgraded.
    l/utf8proc-2.7.0-x86_64-1.txz:  Upgraded.
    n/ca-certificates-20211216-noarch-1.txz:  Upgraded.
      This update provides the latest CA certificates to check for the
      authenticity of SSL connections.
    xap/xine-ui-0.99.13-x86_64-1.txz:  Upgraded.
    extra/php80/php80-8.0.14-x86_64-1.txz:  Upgraded.
    extra/php81/php81-8.1.1-x86_64-1.txz:  Upgraded.
    isolinux/initrd.img:  Rebuilt.
    kernels/*:  Upgraded.
    usb-and-pxe-installers/usbboot.img:  Rebuilt.

--- extra/source/php81/fetch-php.sh
+++ extra/source/php81/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.1.0.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.1.0.tar.xz
+lftpget http://us.php.net/distributions/php-8.1.1.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.1.1.tar.xz

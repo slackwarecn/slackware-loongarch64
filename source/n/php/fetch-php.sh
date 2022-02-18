70040d14e0c7ccb33d58a1cd3df1b8d02eaccdd3
Patrick J Volkerding <volkerdi@slackware.com>
2022-02-18 05:29:00 +0000

    Fri Feb 18 05:29:00 UTC 2022
    a/btrfs-progs-5.16.2-x86_64-1.txz:  Upgraded.
    a/kernel-generic-5.16.10-x86_64-1.txz:  Upgraded.
    a/kernel-huge-5.16.10-x86_64-1.txz:  Upgraded.
    a/kernel-modules-5.16.10-x86_64-1.txz:  Upgraded.
    ap/cups-filters-1.28.12-x86_64-1.txz:  Upgraded.
    ap/qpdf-10.6.2-x86_64-1.txz:  Upgraded.
    d/kernel-headers-5.16.10-x86-1.txz:  Upgraded.
    k/kernel-source-5.16.10-noarch-1.txz:  Upgraded.
      AMD_PTDMA n -> m
      Thanks to walecha.
    l/imagemagick-7.1.0_25-x86_64-1.txz:  Upgraded.
    l/librsvg-2.52.6-x86_64-1.txz:  Upgraded.
    l/pipewire-0.3.46-x86_64-1.txz:  Upgraded.
    n/php-7.4.28-x86_64-1.txz:  Upgraded.
      This update fixes bugs and a security issue:
      UAF due to php_filter_float() failing for ints.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-21708
      (* Security fix *)
    x/libdrm-2.4.110-x86_64-1.txz:  Upgraded.
    xap/mozilla-firefox-97.0.1-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://www.mozilla.org/en-US/firefox/97.0.1/releasenotes/
    xap/mozilla-thunderbird-91.6.1-x86_64-1.txz:  Upgraded.
      This release contains security fixes and improvements.
      For more information, see:
        https://www.mozilla.org/en-US/thunderbird/91.6.1/releasenotes/
        https://www.mozilla.org/en-US/security/advisories/mfsa2022-07/
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2022-0566
      (* Security fix *)
    extra/php80/php80-8.0.16-x86_64-1.txz:  Upgraded.
      This update fixes bugs and a security issue:
      UAF due to php_filter_float() failing for ints.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-21708
      (* Security fix *)
    extra/php81/php81-8.1.3-x86_64-1.txz:  Upgraded.
      This update fixes bugs and a security issue:
      UAF due to php_filter_float() failing for ints.
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-21708
      (* Security fix *)
    isolinux/initrd.img:  Rebuilt.
    kernels/*:  Upgraded.
    usb-and-pxe-installers/usbboot.img:  Rebuilt.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-7.4.27.tar.xz.asc
-lftpget http://us.php.net/distributions/php-7.4.27.tar.xz
+lftpget http://us.php.net/distributions/php-7.4.28.tar.xz.asc
+lftpget http://us.php.net/distributions/php-7.4.28.tar.xz

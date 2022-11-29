31e27c00fbced5522606180f400f04240db154ad
Patrick J Volkerding <volkerdi@slackware.com>
2022-11-29 20:56:03 +0000

    Tue Nov 29 20:56:03 UTC 2022
    a/cryptsetup-2.6.0-x86_64-1.txz:  Upgraded.
    a/kernel-firmware-20221123_cdf9499-noarch-1.txz:  Upgraded.
    kde/bluedevil-5.26.4-x86_64-1.txz:  Upgraded.
    kde/breeze-5.26.4-x86_64-1.txz:  Upgraded.
    kde/breeze-grub-5.26.4-x86_64-1.txz:  Upgraded.
    kde/breeze-gtk-5.26.4-x86_64-1.txz:  Upgraded.
    kde/drkonqi-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kactivitymanagerd-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kde-cli-tools-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kde-gtk-config-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kdecoration-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kdeplasma-addons-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kgamma5-5.26.4-x86_64-1.txz:  Upgraded.
    kde/khotkeys-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kinfocenter-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kmenuedit-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kpipewire-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kscreen-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kscreenlocker-5.26.4-x86_64-1.txz:  Upgraded.
    kde/ksshaskpass-5.26.4-x86_64-1.txz:  Upgraded.
    kde/ksystemstats-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kwallet-pam-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kwayland-integration-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kwin-5.26.4-x86_64-1.txz:  Upgraded.
    kde/kwrited-5.26.4-x86_64-1.txz:  Upgraded.
    kde/layer-shell-qt-5.26.4-x86_64-1.txz:  Upgraded.
    kde/libkscreen-5.26.4-x86_64-1.txz:  Upgraded.
    kde/libksysguard-5.26.4-x86_64-1.txz:  Upgraded.
    kde/milou-5.26.4-x86_64-1.txz:  Upgraded.
    kde/oxygen-5.26.4-x86_64-1.txz:  Upgraded.
    kde/oxygen-sounds-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-browser-integration-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-desktop-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-disks-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-firewall-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-integration-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-nm-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-pa-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-sdk-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-systemmonitor-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-vault-5.26.4-x86_64-1.txz:  Upgraded.
    kde/plasma-workspace-5.26.4.1-x86_64-1.txz:  Upgraded.
    kde/plasma-workspace-wallpapers-5.26.4-x86_64-1.txz:  Upgraded.
    kde/polkit-kde-agent-1-5.26.4-x86_64-1.txz:  Upgraded.
    kde/powerdevil-5.26.4-x86_64-1.txz:  Upgraded.
    kde/qqc2-breeze-style-5.26.4-x86_64-1.txz:  Upgraded.
    kde/sddm-kcm-5.26.4-x86_64-1.txz:  Upgraded.
    kde/systemsettings-5.26.4-x86_64-1.txz:  Upgraded.
    kde/xdg-desktop-portal-kde-5.26.4-x86_64-1.txz:  Upgraded.
    l/glib2-2.74.2-x86_64-2.txz:  Rebuilt.
      Patched to fix C++ API.
      Thanks to 0XBF.
    l/wavpack-5.6.0-x86_64-1.txz:  Upgraded.
    n/iputils-20221126-x86_64-1.txz:  Upgraded.
    n/libmbim-1.28.2-x86_64-1.txz:  Upgraded.
    x/libime-1.0.16-x86_64-1.txz:  Upgraded.
    x/xterm-377-x86_64-1.txz:  Upgraded.
    xap/mozilla-firefox-107.0.1-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://www.mozilla.org/en-US/firefox/107.0.1/releasenotes/

--- recompress.sh
+++ recompress.sh
@@ -792,6 +792,7 @@ gzip ./source/l/netpbm/netpbm-noppmtompeg.patch
 gzip ./source/l/hicolor-icon-theme/doinst.sh
 gzip ./source/l/dvdauthor/dvdauthor-0.7.2_imagemagick7.patch
 gzip ./source/l/glib2/doinst.sh
+gzip ./source/l/glib2/glib.g_str_equal.diff
 gzip ./source/l/gsettings-desktop-schemas/doinst.sh
 gzip ./source/l/gsettings-desktop-schemas/gsettings-desktop-schemas.font.defaults.diff
 gzip ./source/l/gtk+2/gtk.gtk-faq.diff

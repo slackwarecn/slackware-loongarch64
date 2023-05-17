39de96c1694c51da0724ef99b869c8203895dfc9
Patrick J Volkerding <volkerdi@slackware.com>
2023-05-17 20:59:51 +0000

    Wed May 17 20:59:51 UTC 2023
    a/etc-15.1-x86_64-3.txz:  Rebuilt.
      /etc/group: Added kvm (GID 36).
    a/eudev-3.2.12-x86_64-2.txz:  Rebuilt.
      In 50-udev-default.rules, comment out the lines for SGX.
    a/kernel-firmware-20230517_601c181-noarch-1.txz:  Upgraded.
    a/kernel-generic-6.1.29-x86_64-1.txz:  Upgraded.
    a/kernel-huge-6.1.29-x86_64-1.txz:  Upgraded.
    a/kernel-modules-6.1.29-x86_64-1.txz:  Upgraded.
    d/kernel-headers-6.1.29-x86-1.txz:  Upgraded.
    d/llvm-16.0.4-x86_64-1.txz:  Upgraded.
    k/kernel-source-6.1.29-noarch-1.txz:  Upgraded.
    kde/kde-cli-tools-5.27.5.1-x86_64-1.txz:  Upgraded.
    l/fribidi-1.0.13-x86_64-1.txz:  Upgraded.
    l/pipewire-0.3.71-x86_64-1.txz:  Upgraded.
    n/bind-9.18.15-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
    n/curl-8.1.0-x86_64-1.txz:  Upgraded.
      This update fixes security issues:
      more POST-after-PUT confusion.
      IDN wildcard match.
      siglongjmp race condition.
      UAF in SSH sha256 fingerprint check.
      For more information, see:
        https://curl.se/docs/CVE-2023-28322.html
        https://curl.se/docs/CVE-2023-28321.html
        https://curl.se/docs/CVE-2023-28320.html
        https://curl.se/docs/CVE-2023-28319.html
        https://www.cve.org/CVERecord?id=CVE-2023-28322
        https://www.cve.org/CVERecord?id=CVE-2023-28321
        https://www.cve.org/CVERecord?id=CVE-2023-28320
        https://www.cve.org/CVERecord?id=CVE-2023-28319
      (* Security fix *)
    x/libwacom-2.7.0-x86_64-1.txz:  Upgraded.
    isolinux/initrd.img:  Rebuilt.
    kernels/*:  Upgraded.
    usb-and-pxe-installers/usbboot.img:  Rebuilt.

--- recompress.sh
+++ recompress.sh
@@ -137,9 +137,10 @@ gzip ./source/a/acpid/default
 gzip ./source/a/unarj/Makefile
 gzip ./source/a/reiserfsprogs/reiserfsprogs.glibc228.diff
 gzip ./source/a/eudev/60-cdrom_id.rules.diff
-gzip ./source/a/eudev/eudev.no.renderD.no.kvm.diff
 gzip ./source/a/eudev/eudev.ignore_bind_unbind_events.diff
+gzip ./source/a/eudev/eudev.no.sgx.diff
 gzip ./source/a/eudev/doinst.sh
+gzip ./source/a/eudev/eudev.no.renderD.diff
 gzip ./source/a/aaa_libraries/doinst.sh
 gzip ./source/a/gpm/gpm-1.20.1-lib-silent.patch
 gzip ./source/a/gpm/setup.mouse

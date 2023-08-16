4eb488d6f955a0130cc4db4ec8a60e849f117447
Patrick J Volkerding <volkerdi@slackware.com>
2023-08-16 20:45:00 +0000

    Wed Aug 16 20:45:00 UTC 2023
    a/kernel-firmware-20230814_0e048b0-noarch-1.txz:  Upgraded.
    a/kernel-generic-6.1.46-x86_64-1.txz:  Upgraded.
    a/kernel-huge-6.1.46-x86_64-1.txz:  Upgraded.
    a/kernel-modules-6.1.46-x86_64-1.txz:  Upgraded.
    ap/inxi-3.3.29_1-noarch-1.txz:  Upgraded.
    d/kernel-headers-6.1.46-x86-1.txz:  Upgraded.
    k/kernel-source-6.1.46-noarch-1.txz:  Upgraded.
      -ACPI_TINY_POWER_BUTTON n
       ACPI_AC m -> y
       ACPI_BATTERY m -> y
       ACPI_BUTTON m -> y
       ACPI_FAN m -> y
       ACPI_THERMAL m -> y
    kde/kirigami-addons-0.11.0-x86_64-1.txz:  Upgraded.
    n/bind-9.18.18-x86_64-1.txz:  Upgraded.
    n/httpd-2.4.57-x86_64-2.txz:  Rebuilt.
      rc.httpd: wait using pwait after stopping, fix usage to show force-restart.
      Thanks to metaed.
    n/net-snmp-5.9.4-x86_64-1.txz:  Upgraded.
    n/openvpn-2.6.6-x86_64-1.txz:  Upgraded.
    n/php-8.2.9-x86_64-1.txz:  Upgraded.
      This update fixes bugs and security issues:
      Security issue with external entity loading in XML without enabling it.
      Buffer mismanagement in phar_dir_read().
      For more information, see:
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2023-3823
        https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2023-3824
      (* Security fix *)
    x/xorg-server-xwayland-23.2.0-x86_64-1.txz:  Upgraded.
    xap/mozilla-thunderbird-115.1.1-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://www.mozilla.org/en-US/thunderbird/115.1.1/releasenotes/
    isolinux/initrd.img:  Rebuilt.
    kernels/*:  Upgraded.
    usb-and-pxe-installers/usbboot.img:  Rebuilt.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.2.8.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.2.8.tar.xz
+lftpget http://us.php.net/distributions/php-8.2.9.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.2.9.tar.xz

3452074d0869ebcbde1c3850130d5dbb07bd407c
Patrick J Volkerding <volkerdi@slackware.com>
2023-09-21 19:32:42 +0000

    Thu Sep 21 19:32:42 UTC 2023
    a/gettext-0.22.2-x86_64-1.txz:  Upgraded.
    ap/cups-2.4.7-x86_64-1.txz:  Upgraded.
      This update fixes bugs and a security issue:
      Fixed Heap-based buffer overflow when reading Postscript in PPD files.
      For more information, see:
        https://www.cve.org/CVERecord?id=CVE-2023-4504
      (* Security fix *)
    d/cmake-3.27.6-x86_64-1.txz:  Upgraded.
    d/gettext-tools-0.22.2-x86_64-1.txz:  Upgraded.
    l/dconf-editor-45.0.1-x86_64-1.txz:  Upgraded.
    l/gst-plugins-bad-free-1.22.6-x86_64-1.txz:  Upgraded.
    l/gst-plugins-base-1.22.6-x86_64-1.txz:  Upgraded.
    l/gst-plugins-good-1.22.6-x86_64-1.txz:  Upgraded.
    l/gst-plugins-libav-1.22.6-x86_64-1.txz:  Upgraded.
    l/gstreamer-1.22.6-x86_64-1.txz:  Upgraded.
    l/gtk4-4.12.2-x86_64-1.txz:  Upgraded.
    l/imagemagick-7.1.1_17-x86_64-1.txz:  Upgraded.
    n/bind-9.18.19-x86_64-1.txz:  Upgraded.
      This update fixes bugs and security issues:
      Limit the amount of recursion that can be performed by isccc_cc_fromwire.
      Fix use-after-free error in TLS DNS code when sending data.
      For more information, see:
        https://kb.isc.org/docs/cve-2023-3341
        https://www.cve.org/CVERecord?id=CVE-2023-3341
        https://kb.isc.org/docs/cve-2023-4236
        https://www.cve.org/CVERecord?id=CVE-2023-4236
      (* Security fix *)
    n/stunnel-5.71-x86_64-1.txz:  Upgraded.
    x/mesa-23.1.8-x86_64-1.txz:  Upgraded.
    x/xorg-server-xwayland-23.2.1-x86_64-1.txz:  Upgraded.
    xap/freerdp-2.11.2-x86_64-1.txz:  Upgraded.
    xap/mozilla-thunderbird-115.2.3-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://www.mozilla.org/en-US/thunderbird/115.2.3/releasenotes/
    xap/seamonkey-2.53.17.1-x86_64-1.txz:  Upgraded.
      This update contains security fixes and improvements.
      For more information, see:
        https://www.seamonkey-project.org/releases/seamonkey2.53.17.1
        https://www.cve.org/CVERecord?id=CVE-2023-4863
      (* Security fix *)

--- source/l/gst-plugins-bad-free/fetch-sources.sh
+++ source/l/gst-plugins-bad-free/fetch-sources.sh
@@ -26,7 +26,7 @@
 #
 # Example:  VERSION=1.18.5 ./fetch-sources.sh
 
-VERSION=${VERSION:-1.22.5}
+VERSION=${VERSION:-1.22.6}
 
 rm -rf rm -rf gst-plugins-bad-free-$VERSION gst-plugins-bad-$VERSION
 

6011a48b2a96356beb8a567ee93b7fe8c87ace50
Patrick J Volkerding <volkerdi@slackware.com>
2023-12-21 20:46:11 +0000

    Thu Dec 21 20:46:11 UTC 2023
    n/iw-6.7-x86_64-1.txz:  Upgraded.
    n/php-8.3.1-x86_64-1.txz:  Upgraded.
    x/libdrm-2.4.119-x86_64-1.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.3.0.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.3.0.tar.xz
+lftpget http://us.php.net/distributions/php-8.3.1.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.3.1.tar.xz

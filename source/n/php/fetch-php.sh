833db20af85c29d8e60f39c871fd0ef43cd7cccb
Patrick J Volkerding <volkerdi@slackware.com>
2024-01-19 19:58:34 +0000

    Fri Jan 19 19:58:34 UTC 2024
    n/php-8.3.2-x86_64-1.txz:  Upgraded.
    x/wayland-protocols-1.33-noarch-1.txz:  Upgraded.
    xap/network-manager-applet-1.36.0-x86_64-1.txz:  Upgraded.
    xap/xsnow-3.7.7-x86_64-1.txz:  Upgraded.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.3.1.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.3.1.tar.xz
+lftpget http://us.php.net/distributions/php-8.3.2.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.3.2.tar.xz

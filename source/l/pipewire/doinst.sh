8f1367463b7818a6e0ae4f53f705fd86a8717d5c
Patrick J Volkerding <volkerdi@slackware.com>
2024-02-11 22:11:59 +0000

    Sun Feb 11 22:11:59 UTC 2024
    ap/mariadb-10.11.7-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://mariadb.com/kb/en/mariadb-10-11-7-release-notes/
    l/gjs-1.76.3-x86_64-1.txz:  Upgraded.
    l/imagemagick-7.1.1_28-x86_64-1.txz:  Upgraded.
    l/pipewire-1.0.3-x86_64-4.txz:  Rebuilt.
      Use cmp -s in doinst.sh. Thanks to Thom1b and Windu.
    l/wireplumber-0.4.17-x86_64-2.txz:  Rebuilt.
      Use cmp -s in doinst.sh. Thanks to Thom1b and Windu.
    n/dnsmasq-2.89-x86_64-2.txz:  Rebuilt.
      Added trust-anchors.conf and edited PREFIX in dnsmasq.conf to simplify
      setting up DNSSEC. Thanks to marav.
    xap/xsnow-3.7.8-x86_64-1.txz:  Upgraded.

--- source/l/pipewire/doinst.sh
+++ source/l/pipewire/doinst.sh
@@ -1,4 +1,4 @@
 # Toss redundant sample files:
 for file in pipewire.desktop pipewire-pulse.desktop ; do
-  cmp etc/xdg/autostart/${file} etc/xdg/autostart/${file}.sample 2> /dev/null && rm etc/xdg/autostart/${file}.sample
+  cmp -s etc/xdg/autostart/${file} etc/xdg/autostart/${file}.sample && rm etc/xdg/autostart/${file}.sample
 done

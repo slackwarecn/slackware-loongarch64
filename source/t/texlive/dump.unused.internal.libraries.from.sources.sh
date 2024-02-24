0eb4f651daa1f0b4bde7a2497444a30db1f80e2e
Patrick J Volkerding <volkerdi@slackware.com>
2024-02-24 19:50:05 +0000

    Sat Feb 24 19:50:05 UTC 2024
    a/pkgtools-15.1-noarch-10.txz:  Rebuilt.
      setup.services: typo/syntax error fix. Thanks to gramaxo and pghvlaans.
    a/xz-5.6.0-x86_64-1.txz:  Upgraded.
    ap/man-pages-6.06-noarch-2.txz:  Rebuilt.
      Restored the previously included posix pages, and added the posix "sh" page
      since it's more correct than getting the ksh page for "sh".
      Thanks to pghvlaans.
    d/git-2.44.0-x86_64-1.txz:  Upgraded.
    kde/kdnssd-5.115.0-x86_64-2.txz:  Rebuilt.
      Recompiled to add Zeroconf support. (This one fooled me because it doesn't
      actually link to any avahi libraries.)
      Thanks to audriusk.
    kde/kid3-3.9.5-x86_64-1.txz:  Upgraded.
    l/libpng-1.6.43-x86_64-1.txz:  Upgraded.
    l/libunistring-1.2-x86_64-1.txz:  Upgraded.
    n/libksba-1.6.6-x86_64-1.txz:  Upgraded.
    n/npth-1.7-x86_64-1.txz:  Upgraded.
    t/texlive-2023.230322-x86_64-7.txz:  Rebuilt.
      Use the bundled zlib to make the bundled lua happy. Thanks to sombragris.

--- source/t/texlive/dump.unused.internal.libraries.from.sources.sh
+++ source/t/texlive/dump.unused.internal.libraries.from.sources.sh
@@ -5,7 +5,7 @@ VERSION=${VERSION:-$(echo texlive-*source.tar.?z* | rev | cut -f 2 -d - | cut -f
 
 tar xf texlive-${VERSION}-source.tar.xz || exit 1
 mv texlive-${VERSION}-source.tar.xz texlive-${VERSION}-source.tar.xz.orig
-( cd texlive-${VERSION}-source/libs && rm -rf cairo freetype2 gd gmp graphite2 harfbuzz mpfr icu libpng pixman zlib )
+( cd texlive-${VERSION}-source/libs && rm -rf cairo freetype2 gd gmp graphite2 harfbuzz mpfr icu libpng pixman )
 ( cd texlive-${VERSION}-source/utils && rm -rf asymptote )
 ( cd texlive-${VERSION}-source/utils && rm -rf texdoctk )
 ( cd texlive-${VERSION}-source/utils && rm -rf m-tx )

cebf29a6c707c5090f031055f2a523aaa90ed365
Patrick J Volkerding <volkerdi@slackware.com>
2024-02-28 20:43:38 +0000

    Wed Feb 28 20:43:38 UTC 2024
    a/aaa_libraries-15.1-x86_64-27.txz:  Rebuilt.
      Upgraded: liblzma.so.5.6.0, libcares.so.2.12.0, libpng16.so.16.43.0,
      libunistring.so.5.1.0.

--- source/a/aaa_libraries/doinst.sh
+++ source/a/aaa_libraries/doinst.sh
@@ -2,8 +2,6 @@
 # install scripts early on. We used to package symlinks in the normal way, and
 # it's possible that would be fine too, but there must(?) have been a reason
 # that we quit doing that...
-cat var/lib/pkgtools/packages/aaa_libraries* 2> /dev/null | grep -e "^lib" -e "/lib" | grep -v "/$" | while read file ; do
-  if [ -r "$file" ]; then
-    ldconfig -l "$file" 1> /dev/null 2> /dev/null
-  fi
-done
+if [ -x sbin/ldconfig ]; then
+  sbin/ldconfig -r . 2> /dev/null
+fi

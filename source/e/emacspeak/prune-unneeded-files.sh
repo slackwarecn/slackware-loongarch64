2b9bc64077432359d6c3c1cbd555e30e1e1b8ba4
Patrick J Volkerding <volkerdi@slackware.com>
2024-03-04 20:41:42 +0000

    Mon Mar  4 20:41:42 UTC 2024
    d/vala-0.56.15-x86_64-1.txz:  Upgraded.
    e/emacspeak-59.0-x86_64-1.txz:  Upgraded.
    kde/calligra-3.2.1-x86_64-37.txz:  Rebuilt.
      Recompiled against poppler-24.03.0.
    kde/cantor-23.08.5-x86_64-2.txz:  Rebuilt.
      Recompiled against poppler-24.03.0.
    kde/kfilemetadata-5.115.0-x86_64-2.txz:  Rebuilt.
      Recompiled against poppler-24.03.0.
    kde/kile-2.9.93-x86_64-31.txz:  Rebuilt.
      Recompiled against poppler-24.03.0.
    kde/kitinerary-23.08.5-x86_64-2.txz:  Rebuilt.
      Recompiled against poppler-24.03.0.
    kde/krita-5.2.2-x86_64-5.txz:  Rebuilt.
      Recompiled against poppler-24.03.0.
    kde/okular-23.08.5-x86_64-2.txz:  Rebuilt.
      Recompiled against poppler-24.03.0.
    l/poppler-24.03.0-x86_64-1.txz:  Upgraded.
      Shared library .so-version bump.
      Enabled building libpoppler-qt6.
    x/xev-1.2.6-x86_64-1.txz:  Upgraded.
    x/xf86-input-wacom-1.2.1-x86_64-1.txz:  Upgraded.
    x/xfontsel-1.1.1-x86_64-1.txz:  Upgraded.
    x/xmessage-1.0.7-x86_64-1.txz:  Upgraded.
    x/xmh-1.0.5-x86_64-1.txz:  Upgraded.
    x/xpr-1.2.0-x86_64-1.txz:  Upgraded.
    x/xrefresh-1.1.0-x86_64-1.txz:  Upgraded.
    x/xsm-1.0.6-x86_64-1.txz:  Upgraded.

--- source/e/emacspeak/prune-unneeded-files.sh
+++ source/e/emacspeak/prune-unneeded-files.sh
@@ -8,12 +8,6 @@ VERSION=${VERSION:-$(echo $PKGNAM-*.tar.bz2 | rev | cut -f 3- -d . | cut -f 1 -d
 rm -rf ${PKGNAM}-${VERSION}
 tar xf ${PKGNAM}-${VERSION}.tar.bz2 || exit 1
 
-# I have no idea what these are useful for. They aren't referenced anywhere
-# else. If .json weren't such a bloated format I might keep them just in case,
-# but let's strip them out for now. If it would be better to keep them (perhaps
-# compressed as .gz or .xz), let me know.
-( cd ${PKGNAM}-${VERSION}/etc && rm -f CompSciFact.json TVRaman.json UnixToolTip.json )
-
 # Get rid of support files used to build .html:
 ( cd ${PKGNAM}-${VERSION}/etc
   for file in *.html ; do
@@ -33,9 +27,6 @@ tar xf ${PKGNAM}-${VERSION}.tar.bz2 || exit 1
   mv news-tmp/* .
   rmdir news-tmp
 )
-
-# Drop the Makefile in etc/:
-rm -f ${PKGNAM}-${VERSION}/etc/Makefile
 
 # More cruft:
 rm -rf ${PKGNAM}-${VERSION}/.ccls-cache

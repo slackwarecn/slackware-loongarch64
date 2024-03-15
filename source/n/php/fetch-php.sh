4a56bfa5c61b68011688679d66ee94000c93274f
Patrick J Volkerding <volkerdi@slackware.com>
2024-03-15 18:55:58 +0000

    Fri Mar 15 18:55:58 UTC 2024
    d/mercurial-6.7-x86_64-1.txz:  Upgraded.
    kde/digikam-8.3.0-x86_64-1.txz:  Upgraded.
    l/libxml2-2.12.6-x86_64-1.txz:  Upgraded.
    n/php-8.3.4-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://www.php.net/ChangeLog-8.php#8.3.4
    n/proftpd-1.3.8b-x86_64-3.txz:  Rebuilt.
      Added mod_ldap. Thanks to Thom1b.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.3.3.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.3.3.tar.xz
+lftpget http://us.php.net/distributions/php-8.3.4.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.3.4.tar.xz

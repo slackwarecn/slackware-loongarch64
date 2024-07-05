e61df665da5a1e5b181d42ae12bb0dad914a47b2
Patrick J Volkerding <volkerdi@slackware.com>
2024-07-05 20:15:51 +0000

    Fri Jul  5 20:15:51 UTC 2024
    a/kernel-firmware-20240703_e94a2a3-noarch-1.txz:  Upgraded.
    a/kernel-generic-6.9.8-x86_64-1.txz:  Upgraded.
    a/kernel-huge-6.9.8-x86_64-1.txz:  Upgraded.
    a/kernel-modules-6.9.8-x86_64-1.txz:  Upgraded.
    d/kernel-headers-6.9.8-x86-1.txz:  Upgraded.
    d/luajit-2.1.1720049189-x86_64-1.txz:  Upgraded.
    k/kernel-source-6.9.8-noarch-1.txz:  Upgraded.
    l/libxslt-1.1.42-x86_64-1.txz:  Upgraded.
    n/NetworkManager-1.48.4-x86_64-1.txz:  Upgraded.
    n/c-ares-1.32.0-x86_64-1.txz:  Upgraded.
    n/p11-kit-0.25.5-x86_64-1.txz:  Upgraded.
    n/php-8.3.9-x86_64-1.txz:  Upgraded.
      This is a bugfix release.
      For more information, see:
        https://www.php.net/ChangeLog-8.php#8.3.9
    n/wireless-regdb-2024.07.04-x86_64-1.txz:  Upgraded.
    isolinux/initrd.img:  Rebuilt.
    kernels/*:  Upgraded.
    usb-and-pxe-installers/usbboot.img:  Rebuilt.

--- source/n/php/fetch-php.sh
+++ source/n/php/fetch-php.sh
@@ -1,2 +1,2 @@
-lftpget http://us.php.net/distributions/php-8.3.8.tar.xz.asc
-lftpget http://us.php.net/distributions/php-8.3.8.tar.xz
+lftpget http://us.php.net/distributions/php-8.3.9.tar.xz.asc
+lftpget http://us.php.net/distributions/php-8.3.9.tar.xz

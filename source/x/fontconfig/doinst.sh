367b1180dacbc8a8819aed49bc72d779a911ed5f
Patrick J Volkerding <volkerdi@slackware.com>
2024-11-28 04:21:57 +0000

    Thu Nov 28 04:21:57 UTC 2024
    x/fontconfig-2.15.0-x86_64-2.txz:  Rebuilt.
      Prefer Liberation for sans and serif (as usual). Thanks to Daedra.
      Migrate files from /etc/fonts/conf.avail to /usr/share/fontconfig/fonts.avail.

--- source/x/fontconfig/doinst.sh
+++ source/x/fontconfig/doinst.sh
@@ -4,3 +4,19 @@ if [ -x /usr/bin/fc-cache ]; then
 fi
 # else we'll catch it later with setup.fontconfig :-)
 
+# With fontconfig-2.15.0 we use the default of /usr/share/fontconfig/conf.avail
+# so if we find any of "our" files in the old location, it's possible that they
+# rode the old symlink there prior to this package being installed. Let's make
+# sure that they get home OK.
+if [ -d etc/fonts/conf.avail ]; then
+  grep usr/share/fontconfig/conf.avail/ var/lib/pkgtools/packages/* | cut -f 2 -d : | sort | uniq | grep -v "/$" | while read conf ; do
+    etcconf="etc/fonts/$(echo $conf | cut -f 4- -d /)"
+    if [ ! -r $conf -a -r $etcconf ]; then
+      mv $etcconf $conf
+    else
+      rm -f $etcconf
+    fi
+  done
+fi
+# If we can, get rid of this. Could be held by third-party packages though.
+rmdir etc/fonts/conf.avail 2> /dev/null

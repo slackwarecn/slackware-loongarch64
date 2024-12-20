3db81db0b70cc9d3fcd7d158027d941f30df92fd
Patrick J Volkerding <volkerdi@slackware.com>
2024-12-20 20:45:00 +0000

    Fri Dec 20 20:45:00 UTC 2024
    a/dbus-1.16.0-x86_64-1.txz:  Upgraded.
      Thanks to gmgf for the elogind patch.
    ap/lxc-6.0.3-x86_64-1.txz:  Upgraded.
    d/cmake-3.31.3-x86_64-1.txz:  Upgraded.
    l/libwebp-1.5.0-x86_64-1.txz:  Upgraded.
    l/python-psutil-6.1.1-x86_64-1.txz:  Upgraded.

--- recompress.sh
+++ recompress.sh
@@ -309,8 +309,6 @@ gzip ./source/a/bash/bash_manpages.patch
 gzip ./source/a/bash/doinst.sh
 gzip ./source/a/dbus/doinst.sh
 gzip ./source/a/dbus/rc.messagebus
-gzip ./source/a/dbus/dbus-1.12.x-allow_root_globally.diff
-gzip ./source/a/dbus/dbus-enable-elogind.patch
 gzip ./source/a/mcelog/mcelog.logrotate.size.syntax.diff
 gzip ./source/a/mcelog/mcelog.init.diff
 gzip ./source/a/mcelog/doinst.sh

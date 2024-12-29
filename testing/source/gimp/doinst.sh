b5534e9d8923426978c9b9786af388c9435db6b7
Patrick J Volkerding <volkerdi@slackware.com>
2024-12-29 19:49:35 +0000

    Sun Dec 29 19:49:35 UTC 2024
    kde/extra-cmake-modules-6.9.0-x86_64-1.txz:  Upgraded.
      There are a few things out there that need the newer ECM, but don't actually
      need much else from Plasma 6 yet. Anyway, don't read too much into this. ;-)
      KF5 / Plasma 5 can build using the newer ECM just fine.
      Requested by Heinz Wiesinger.
    l/appstream-glib-0.8.3-x86_64-1.txz:  Added.
    l/luajit-lgi-0.9.2-x86_64-1.txz:  Added.
    l/netpbm-11.09.02-x86_64-1.txz:  Upgraded.
    xfce/xfce4-xkb-plugin-0.8.5-x86_64-1.txz:  Upgraded.
    testing/packages/gimp-3.0.0_RC2-x86_64-1.txz:  Added.

---
+++ testing/source/gimp/doinst.sh
@@ -0,0 +1,15 @@
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+for file in etc/gimp/3.0/*.new ; do
+  config $file
+done

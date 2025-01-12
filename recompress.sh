8c57f2abd31c87400c1769c8a97ba1008191ae25
Patrick J Volkerding <volkerdi@slackware.com>
2025-01-12 19:23:46 +0000

    Sun Jan 12 19:23:46 UTC 2025
    a/mkinitrd-1.4.11-x86_64-62.txz:  Rebuilt.
      setup.01.mkinitrd: use a simple grep match to test the kernel, not "file".
      Thanks to Mechanikx.
    l/libcdio-2.2.0-x86_64-1.txz:  Upgraded.
      Shared library .so-version bump.
    l/libsoup3-3.6.3-x86_64-1.txz:  Upgraded.
    l/python-typogrify-2.1.0-x86_64-1.txz:  Upgraded.
    n/netatalk-4.1.0-x86_64-1.txz:  Upgraded.

--- recompress.sh
+++ recompress.sh
@@ -663,7 +663,6 @@ gzip ./source/l/gtk+/gtk+-1.2.10-clistfocusrow.patch
 gzip ./source/l/gtk+/gtk+-1.2.10-troughpaint.patch
 gzip ./source/l/gtk+/gtk+-1.2.10-ukfont.patch
 gzip ./source/l/gtk+/gtk+-1.2.10-fontwarning.patch
-gzip ./source/l/python-typogrify/jinja-3.1.patch
 gzip ./source/l/db48/db.rename.atomic_compare_exchange.diff
 gzip ./source/l/db48/db48.no.broken.doc.install.diff
 gzip ./source/l/boost/boost.bootstrap.python3.diff

11e9f49bfea64578776840854aadf154430dbd38
Patrick J Volkerding <volkerdi@slackware.com>
2025-03-06 23:19:52 +0000

    Thu Mar  6 23:19:52 UTC 2025
    d/llvm-20.1.0-x86_64-1.txz:  Upgraded.
      Shared library .so-version bump.
    kde/kdevelop-23.08.5-x86_64-4.txz:  Rebuilt.
      Recompiled against llvm-20.1.0.
    l/gtk+3-3.24.49-x86_64-1.txz:  Upgraded.
    l/libclc-20.1.0-x86_64-1.txz:  Upgraded.
      Compiled against llvm-20.1.0.
    l/qt5-5.15.16_20250122_d76aaaf8-x86_64-2.txz:  Rebuilt.
      Recompiled against llvm-20.1.0.
    l/qt6-6.8.2_20250124_480041bb-x86_64-5.txz:  Rebuilt.
      Recompiled against llvm-20.1.0.
    l/spirv-llvm-translator-20.1.0-x86_64-1.txz:  Upgraded.
      Compiled against llvm-20.1.0.
    x/mesa-25.0.1-x86_64-1.txz:  Upgraded.
      Compiled against llvm-20.1.0.

--- recompress.sh
+++ recompress.sh
@@ -1014,5 +1014,4 @@ gzip ./source/x/ibus/doinst.sh
 gzip ./source/x/ibus/emoji-test.txt
 gzip ./source/x/xcm/xcm.udev.rules.diff
 gzip ./source/x/libva/doinst.sh
-gzip ./testing/source/mesa/doinst.sh
 gzip ./testing/source/linux-6.13.x/patches-revert-i686/0001-x86-ioremap-Fix-page-aligned-size-calculation-in-__i.patch
